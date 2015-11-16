9fb8c66fbddf072edd2ab6a797562b1dc3d3bdd9
lynntyx <Lynn@Yinxings-MacBook-Pro.local>
2015-11-16 03:47:38 -0800

    music

---
+++ project3/script-font.js
@@ -0,0 +1,762 @@
+$(document).ready(function(){
+
+    /* Load the audios */
+
+    var audioA = document.createElement('audio');
+    audioA.setAttribute('src', 'piano/A.mp3');
+
+    var audioB = document.createElement('audio');
+    audioB.setAttribute('src', 'piano/B.mp3');
+
+    var audioC = document.createElement('audio');
+    audioC.setAttribute('src', 'piano/C.mp3');
+
+    var audioD = document.createElement('audio');
+    audioD.setAttribute('src', 'piano/D.mp3');
+
+    var audioE = document.createElement('audio');
+    audioE.setAttribute('src', 'piano/E.mp3');
+
+    var audioF = document.createElement('audio');
+    audioF.setAttribute('src', 'piano/F.mp3');
+
+    var audioG = document.createElement('audio');
+    audioG.setAttribute('src', 'piano/G.mp3');
+
+    var audioH = document.createElement('audio');
+    audioH.setAttribute('src', 'piano/H.mp3');
+
+    var audioI = document.createElement('audio');
+    audioI.setAttribute('src', 'piano/I.mp3');
+
+    var audioJ = document.createElement('audio');
+    audioJ.setAttribute('src', 'piano/J.mp3');
+
+    var audioK = document.createElement('audio');
+    audioK.setAttribute('src', 'piano/K.mp3');
+
+    var audioL = document.createElement('audio');
+    audioL.setAttribute('src', 'piano/L.mp3');
+
+    var audioM = document.createElement('audio');
+    audioM.setAttribute('src', 'piano/M.mp3');
+
+    var audioN = document.createElement('audio');
+    audioN.setAttribute('src', 'piano/N.mp3');
+
+    var audioO = document.createElement('audio');
+    audioO.setAttribute('src', 'piano/O.mp3');
+
+    var audioP = document.createElement('audio');
+    audioP.setAttribute('src', 'piano/P.mp3');
+
+    var audioQ = document.createElement('audio');
+    audioQ.setAttribute('src', 'piano/Q.mp3');
+
+    var audioR = document.createElement('audio');
+    audioR.setAttribute('src', 'piano/R.mp3');
+
+    var audioS = document.createElement('audio');
+    audioS.setAttribute('src', 'piano/S.mp3');
+
+    var audioT = document.createElement('audio');
+    audioT.setAttribute('src', 'piano/T.mp3');
+
+    var audioU = document.createElement('audio');
+    audioU.setAttribute('src', 'piano/U.mp3');
+
+    var audioV = document.createElement('audio');
+    audioV.setAttribute('src', 'piano/V.mp3');
+
+    var audioW = document.createElement('audio');
+    audioW.setAttribute('src', 'piano/W.mp3');
+
+    var audioX = document.createElement('audio');
+    audioX.setAttribute('src', 'piano/X.mp3');
+
+    var audioY = document.createElement('audio');
+    audioY.setAttribute('src', 'piano/Y.mp3');
+
+    var audioZ = document.createElement('audio');
+    audioZ.setAttribute('src', 'piano/Z.mp3');
+
+    var audioP1 = document.createElement('audio');
+    audioP1.setAttribute('src', 'piano/period.mp3');
+
+    var audioC1 = document.createElement('audio');
+    audioC1.setAttribute('src', 'piano/comma.mp3');
+
+    var audio1 = document.createElement('audio');
+    audio1.setAttribute('src', 'piano/1.mp3');
+
+    var audio2 = document.createElement('audio');
+    audio2.setAttribute('src', 'piano/2.mp3');
+
+    var audio3 = document.createElement('audio');
+    audio3.setAttribute('src', 'piano/3.mp3');
+
+    var audio4 = document.createElement('audio');
+    audio4.setAttribute('src', 'piano/4.mp3');
+
+    var audio5 = document.createElement('audio');
+    audio5.setAttribute('src', 'piano/5.mp3');
+
+    var audio6 = document.createElement('audio');
+    audio6.setAttribute('src', 'piano/6.mp3');
+
+    var audio7 = document.createElement('audio');
+    audio7.setAttribute('src', 'piano/7.mp3');
+
+    var audio8 = document.createElement('audio');
+    audio8.setAttribute('src', 'piano/8.mp3');
+
+    var audio9 = document.createElement('audio');
+    audio9.setAttribute('src', 'piano/9.mp3');
+
+    var audio0 = document.createElement('audio');
+    audio0.setAttribute('src', 'piano/0.mp3');
+
+    $.get();
+
+    audioA.addEventListener("load", function() {
+        audioA.play();
+    }, true);
+
+    audioB.addEventListener("load", function() {
+        audioB.play();
+    }, true);
+
+    audioC.addEventListener("load", function() {
+        audioC.play();
+    }, true);
+
+    audioD.addEventListener("load", function() {
+        audioD.play();
+    }, true);
+
+    audioE.addEventListener("load", function() {
+        audioE.play();
+    }, true);
+
+    audioF.addEventListener("load", function() {
+        audioF.play();
+    }, true);
+
+    audioG.addEventListener("load", function() {
+        audioG.play();
+    }, true);
+
+    audioH.addEventListener("load", function() {
+        audioH.play();
+    }, true);
+
+    audioI.addEventListener("load", function() {
+        audioI.play();
+    }, true);
+
+    audioJ.addEventListener("load", function() {
+        audioJ.play();
+    }, true);
+
+    audioK.addEventListener("load", function() {
+        audioK.play();
+    }, true);
+
+    audioL.addEventListener("load", function() {
+        audioL.play();
+    }, true);
+
+    audioM.addEventListener("load", function() {
+        audioM.play();
+    }, true);
+
+    audioN.addEventListener("load", function() {
+        audioN.play();
+    }, true);
+
+    audioO.addEventListener("load", function() {
+        audioO.play();
+    }, true);
+
+    audioO.addEventListener("load", function() {
+        audioP.play();
+    }, true);
+
+    audioQ.addEventListener("load", function() {
+        audioQ.play();
+    }, true);
+
+    audioR.addEventListener("load", function() {
+        audioR.play();
+    }, true);
+
+    audioS.addEventListener("load", function() {
+        audioS.play();
+    }, true);
+
+    audioT.addEventListener("load", function() {
+        audioT.play();
+    }, true);
+
+    audioU.addEventListener("load", function() {
+        audioU.play();
+    }, true);
+
+    audioV.addEventListener("load", function() {
+        audioV.play();
+    }, true);
+
+    audioW.addEventListener("load", function() {
+        audioW.play();
+    }, true);
+
+    audioX.addEventListener("load", function() {
+        audioX.play();
+    }, true);
+
+    audioY.addEventListener("load", function() {
+        audioY.play();
+    }, true);
+
+    audioZ.addEventListener("load", function() {
+        audioZ.play();
+    }, true);
+
+    audioP1.addEventListener("load", function() {
+        audioP1.play();
+    }, true);
+
+    audioC1.addEventListener("load", function() {
+        audioC1.play();
+    }, true);
+
+    audio1.addEventListener("load", function() {
+        audio1.play();
+    }, true);
+
+    audio2.addEventListener("load", function() {
+        audio2.play();
+    }, true);
+
+    audio3.addEventListener("load", function() {
+        audio3.play();
+    }, true);
+
+    audio4.addEventListener("load", function() {
+        audio4.play();
+    }, true);
+
+    audio5.addEventListener("load", function() {
+        audio5.play();
+    }, true);
+
+    audio6.addEventListener("load", function() {
+        audio6.play();
+    }, true);
+
+    audio7.addEventListener("load", function() {
+        audio7.play();
+    }, true);
+
+    audio8.addEventListener("load", function() {
+        audio8.play();
+    }, true);
+
+    audio9.addEventListener("load", function() {
+        audio9.play();
+    }, true);
+
+    audio0.addEventListener("load", function() {
+        audio0.play();
+    }, true);
+
+
+    /* What happens when you click letters... */
+
+    $('html').keydown(function(e){
+
+    // letter a
+    if(e.keyCode == 65){
+      audioA.play();
+      $('.A').addClass('pressed');
+    }
+
+    // letter b
+    if(e.keyCode == 66){
+      audioB.play();
+      $('.B').addClass('pressed');
+    }
+
+    // letter c
+    if(e.keyCode == 67){
+      audioC.play();
+      $('.C').addClass('pressed');
+    }
+
+    // letter d
+    if(e.keyCode == 68){
+      audioD.play();
+      $('.D').addClass('pressed');
+    }
+
+    // letter e
+    if(e.keyCode == 69){
+      audioE.play();
+      $('.E').addClass('pressed');
+    }
+
+    // letter f
+    if(e.keyCode == 70){
+      audioF.play();
+      $('.F').addClass('pressed');
+    }
+
+    // letter g
+    if(e.keyCode == 71){
+      audioG.play();
+      $('.G').addClass('pressed');
+    }
+
+    // letter h
+    if(e.keyCode == 72){
+      audioH.play();
+      $('.H').addClass('pressed');
+    }
+
+    // letter i
+    if(e.keyCode == 73){
+      audioI.play();
+      $('.I').addClass('pressed');
+    }
+
+    // letter j
+    if(e.keyCode == 74){
+      audioJ.play();
+      $('.J').addClass('pressed');
+    }
+
+    // letter k
+    if(e.keyCode == 75){
+      audioK.play();
+      $('.K').addClass('pressed');
+    }
+
+    // letter l
+    if(e.keyCode == 76){
+      audioL.play();
+      $('.L').addClass('pressed');
+    }
+
+    // letter m
+    if(e.keyCode == 77){
+      audioM.play();
+      $('.M').addClass('pressed');
+    }
+
+    // letter n
+    if(e.keyCode == 78){
+      audioN.play();
+      $('.N').addClass('pressed');
+    }
+
+    // letter o
+    if(e.keyCode == 79){
+      audioO.play();
+      $('.O').addClass('pressed');
+    }
+
+    // letter p
+    if(e.keyCode == 80){
+      audioP.play();
+      $('.P').addClass('pressed');
+    }
+
+    // letter q
+    if(e.keyCode == 81){
+      audioQ.play();
+      $('.Q').addClass('pressed');
+    }
+
+    // letter r
+    if(e.keyCode == 82){
+      audioR.play();
+      $('.R').addClass('pressed');
+    }
+
+    // letter s
+    if(e.keyCode == 83){
+      audioS.play();
+      $('.S').addClass('pressed');
+    }
+
+    // letter t
+    if(e.keyCode == 84){
+      audioT.play();
+      $('.T').addClass('pressed');
+    }
+
+    // letter u
+    if(e.keyCode == 85){
+      audioU.play();
+      $('.U').addClass('pressed');
+    }
+
+    // letter v
+    if(e.keyCode == 86){
+      audioV.play();
+      $('.V').addClass('pressed');
+    }
+
+    // letter w
+    if(e.keyCode == 87){
+      audioW.play();
+      $('.W').addClass('pressed');
+    }
+
+    // letter x
+    if(e.keyCode == 88){
+      audioX.play();
+      $('.X').addClass('pressed');
+    }
+
+    // letter y
+    if(e.keyCode == 89){
+      audioY.play();
+      $('.Y').addClass('pressed');
+    }
+
+// letter z
+    if(e.keyCode == 90){
+      audioZ.play();
+      $('.Z').addClass('pressed');
+    }
+
+    // punctuation .
+    if(e.keyCode == 190){
+      audioP1.play();
+      $('.P1').addClass('pressed');
+    }
+
+    // punctuation ,
+    if(e.keyCode == 188){
+      audioC1.play();
+      $('.C1').addClass('pressed');
+    }
+
+    // letter 1
+    if(e.keyCode == 49){
+      audio1.play();
+      $('.1').addClass('pressed');
+    }
+
+    // letter 2
+    if(e.keyCode == 50){
+      audio2.play();
+      $('.2').addClass('pressed');
+    }
+
+    // letter 3
+    if(e.keyCode == 51){
+      audio3.play();
+      $('.3').addClass('pressed');
+    }
+
+    // letter 4
+    if(e.keyCode == 52){
+      audio4.play();
+      $('.4').addClass('pressed');
+    }
+
+    // letter 5
+    if(e.keyCode == 53){
+      audio5.play();
+      $('.5').addClass('pressed');
+    }
+
+    // letter 6
+    if(e.keyCode == 54){
+      audio6.play();
+      $('.6').addClass('pressed');
+    }
+
+    // letter 7
+    if(e.keyCode == 55){
+      audio7.play();
+      $('.7').addClass('pressed');
+    }
+
+    // letter 8
+    if(e.keyCode == 56){
+      audio8.play();
+      $('.8').addClass('pressed');
+    }
+
+    // letter 9
+    if(e.keyCode == 57){
+      audio9.play();
+      $('.9').addClass('pressed');
+    }
+
+    // letter 0
+    if(e.keyCode == 48){
+      audio0.play();
+      $('.0').addClass('pressed');
+    }
+
+
+
+    });
+
+    $('html').keyup(function(e){
+      $('*').removeClass('pressed');
+      
+      // letter a
+      if(e.keyCode == 65){
+        audioA.pause();
+        audioA.currentTime = 0;
+      }
+
+      // letter b
+      if(e.keyCode == 66){
+        audioB.pause();
+        audioB.currentTime = 0;
+      }
+
+      // letter c
+      if(e.keyCode == 67){
+        audioC.pause();
+        audioC.currentTime = 0;
+      }
+
+      // letter d
+      if(e.keyCode == 68){
+        audioD.pause();
+        audioD.currentTime = 0;
+      }
+
+      // letter e
+      if(e.keyCode == 69){
+        audioE.pause();
+        audioE.currentTime = 0;
+      }
+
+      // letter f
+      if(e.keyCode == 70){
+        audioF.pause();
+        audioF.currentTime = 0;
+      }
+
+      // letter g
+      if(e.keyCode == 71){
+        audioG.pause();
+        audioG.currentTime = 0;
+      }
+
+      // letter h
+      if(e.keyCode == 72){
+        audioH.pause();
+        audioH.currentTime = 0;
+      }
+
+      // letter i
+      if(e.keyCode == 73){
+        audioI.pause();
+        audioI.currentTime = 0;
+      }
+
+      // letter j
+      if(e.keyCode == 74){
+        audioJ.pause();
+        audioJ.currentTime = 0;
+      }
+
+      // letter k
+      if(e.keyCode == 75){
+        audioK.pause();
+        audioK.currentTime = 0;
+      }
+
+      // letter l
+      if(e.keyCode == 76){
+        audioL.pause();
+        audioL.currentTime = 0;
+      }
+
+      // letter m
+      if(e.keyCode == 77){
+        audioM.pause();
+        audioM.currentTime = 0;
+      }
+
+      // letter n
+      if(e.keyCode == 78){
+        audioN.pause();
+        audioN.currentTime = 0;
+      }
+
+      // letter o
+      if(e.keyCode == 79){
+        audioO.pause();
+        audioO.currentTime = 0;
+      }
+
+      // letter p
+      if(e.keyCode == 80){
+        audioP.pause();
+        audioP.currentTime = 0;
+      }
+
+      // letter q
+      if(e.keyCode == 81){
+        audioQ.pause();
+        audioQ.currentTime = 0;
+      }
+
+      // letter r
+      if(e.keyCode == 82){
+        audioR.pause();
+        audioR.currentTime = 0;
+      }
+
+      // letter s
+      if(e.keyCode == 83){
+        audioS.pause();
+        audioS.currentTime = 0;
+      }
+
+      // letter t
+      if(e.keyCode == 84){
+        audioT.pause();
+        audioT.currentTime = 0;
+      }
+
+      // letter u
+      if(e.keyCode == 85){
+        audioU.pause();
+        audioU.currentTime = 0;
+      }
+
+      // letter v
+      if(e.keyCode == 86){
+        audioV.pause();
+        audioV.currentTime = 0;
+      }
+
+      // letter w
+      if(e.keyCode == 87){
+        audioW.pause();
+        audioW.currentTime = 0;
+      }
+
+      // letter x
+      if(e.keyCode == 88){
+        audioX.pause();
+        audioX.currentTime = 0;
+      }
+
+      // letter y
+      if(e.keyCode == 89){
+        audioY.pause();
+        audioY.currentTime = 0;
+      }
+
+      // letter z
+      if(e.keyCode == 90){
+        audioZ.pause();
+        audioZ.currentTime = 0;
+      }
+
+      // punctuation .
+      if(e.keyCode == 190){
+        audioP1.pause();
+        audioP1.currentTime = 0;
+      }
+
+      // punctuation ,
+      if(e.keyCode == 188){
+        audioC1.pause();
+        audioC1.currentTime = 0;
+      }
+
+      // letter 1
+      if(e.keyCode == 49){
+        audio1.pause();
+        audio1.currentTime = 0;
+      }
+
+      // letter 2
+      if(e.keyCode == 50){
+        audio2.pause();
+        audio2.currentTime = 0;
+      }
+
+      // letter 3
+      if(e.keyCode == 51){
+        audio3.pause();
+        audio3.currentTime = 0;
+      }
+
+      // letter 4
+      if(e.keyCode == 52){
+        audio4.pause();
+        audio4.currentTime = 0;
+      }
+
+      // letter 5
+      if(e.keyCode == 53){
+        audio5.pause();
+        audio5.currentTime = 0;
+      }
+
+      // letter 6
+      if(e.keyCode == 54){
+        audio6.pause();
+        audio6.currentTime = 0;
+      }
+
+      // letter 7
+      if(e.keyCode == 55){
+        audio7.pause();
+        audio7.currentTime = 0;
+      }
+
+      // letter 8
+      if(e.keyCode == 56){
+        audio8.pause();
+        audio8.currentTime = 0;
+      }
+
+      // letter 9
+      if(e.keyCode == 57){
+        audio9.pause();
+        audio9.currentTime = 0;
+      }
+
+      // letter 0
+      if(e.keyCode == 48){
+        audio0.pause();
+        audio0.currentTime = 0;
+      }
+
+    });
+
+
+    // $('.circle').click(function() {
+
+    //     if (audioCircle.paused == true) {
+    //       audioCircle.play();
+    //       $(this).closest('.letter-container').css('background', '#632C97');
+    //     }
+
+    //     else {
+    //       audioCircle.pause();
+    //       audioCircle.currentTime = 0;
+    //       audioCircle.play();
+    //     }
+
+        // audioCircle.on("ended", $(this).removeClass('animate-flicker'));
+
+    // });
+
+
+});
